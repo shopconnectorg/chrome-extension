5330213ecc0459fda81a344e90b87bd0dccd74f2
Denis Perov <denis.perov@gmail.com>
2023-10-16 23:02:47 +0600

    Implement requests auto approving (experiment)

--- src/App.js
+++ src/App.js
@@ -2,21 +2,41 @@ import React, { useEffect, useState } from 'react';
 import { Home, Welcome, Auth, NewAccount } from './pages';
 import { Routes, Route } from 'react-router-dom';
 import { ExtensionService } from './services/Extension.service';
+import { IdentityService } from "./services/Identity.service";
 import './App.css';
 import CircularProgress from '@mui/material/CircularProgress';
+
+async function ensureAccount() {
+  const accounts = JSON.parse(localStorage.getItem("accounts"));
+  if (!accounts || accounts.length <= 0) {
+    const { did } = await IdentityService.createIdentity();
+    localStorage.setItem(
+      "accounts",
+      JSON.stringify([
+        {
+          name: "Automatic",
+          did: did.string(),
+          isActive: true,
+        },
+      ])
+    );
+  }
+}
 
 function App() {
   const [inited, setInited] = useState(false);
   const [error, setError] = useState('');
-  
-  useEffect(()=>{
-    ExtensionService.getInstance()
-      .then(() => setInited(true))
-      .catch(err => {
-        setError(err.message)
-        console.error(err);
-      });
-  }, [])
+
+  useEffect(() => {
+    (async () => {
+      await ExtensionService.getInstance();
+      await ensureAccount();
+      setInited(true);
+    })().catch((err) => {
+      setError(err.message);
+      console.error(err)
+    });
+  }, []);
 
   return (
     <div className="App">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { LinearProgress } from "@mui/material";
 import { HeaderComponent } from "../components/app-header";
 import { AccountInfo } from "../components/account-info";
 import { CredentialsInfo } from "../components/credentials";
-import { useNavigate } from "react-router-dom";
 import { ExtensionService } from "../services/Extension.service";
+
 export const Home = () => {
-  const navigate = useNavigate();
-  const [accounts, setAccounts] = useState([]);
   const [credentials, setCredentials] = useState([]);
+  const accounts = JSON.parse(localStorage.getItem("accounts"))
 
   const getCredentials = async () => {
     const { credWallet } = await ExtensionService.getInstance();
@@ -17,22 +17,7 @@ export const Home = () => {
   };
 
   useEffect(() => {
-	  const accounts = JSON.parse(localStorage.getItem('accounts'));
-	  if(!accounts || accounts.length <= 0) {
-		  navigate('/welcome');
-	  } else {
-		  setAccounts(accounts);
-	  }
-    window.addEventListener("storage", () => {
-      console.log("Change to local storage!");
-      const accounts = JSON.parse(localStorage.getItem("accounts"));
-      setAccounts(accounts ? accounts : []);
-      if(!accounts || accounts.length <= 0) {
-        navigate('/welcome');
-      }
-    });
-	  getCredentials().catch(console.error);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    getCredentials().catch(console.error);
   }, []);
 
   const handleCredentialDelete = async (credentialId) => {
@@ -43,16 +28,21 @@ export const Home = () => {
 
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
-	  { accounts.length <=0 && <p>Redirecting...</p> }
-	  { accounts.length > 0 && <div>
-        <HeaderComponent />
-        <AccountInfo accounts={accounts} />
-        <CredentialsInfo
-          credentials={credentials}
-          onDeleteCredential={handleCredentialDelete}
-        />
-	  </div>
-	  }
+      {accounts?.length > 0 ? (
+        <div>
+          <HeaderComponent />
+          <AccountInfo accounts={accounts} />
+          <CredentialsInfo
+            credentials={credentials}
+            onDeleteCredential={handleCredentialDelete}
+          />
+        </div>
+      ) : (
+        <div className="progress-indicator">
+          <h2>Initalizing...</h2>
+          <LinearProgress size={20} />
+        </div>
+      )}
     </div>
   );
 };
