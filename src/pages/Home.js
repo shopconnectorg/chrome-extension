9a44bbad93c53179d98d566551aee2391439d468
Denis Perov <denis.perov@gmail.com>
2023-10-17 22:12:07 +0600

    Credentials filtering demo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { LinearProgress, TextField } from "@mui/material";
 import { HeaderComponent } from "../components/app-header";
 import { AccountInfo } from "../components/account-info";
 import { CredentialsInfo } from "../components/credentials";
@@ -8,12 +9,21 @@ export const Home = () => {
   const navigate = useNavigate();
   const [accounts, setAccounts] = useState([]);
   const [credentials, setCredentials] = useState([]);
+  const [filter, setFilter] = useState(null);
 
   const getCredentials = async () => {
     const { credWallet } = await ExtensionService.getInstance();
-    // todo find by query
-    const credentials = await credWallet.list();
-    setCredentials(credentials);
+    if (filter) {
+      try {
+        const credentials = await credWallet.findByQuery(JSON.parse(filter));
+        setCredentials(credentials);
+      } catch (err) {
+        setCredentials([]);
+      }
+    } else {
+      const credentials = await credWallet.list();
+      setCredentials(credentials);
+    }
   };
 
   useEffect(() => {
@@ -33,7 +43,7 @@ export const Home = () => {
     });
 	  getCredentials().catch(console.error);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [filter]);
 
   const handleCredentialDelete = async (credentialId) => {
     const { credWallet } = await ExtensionService.getInstance();
@@ -43,16 +53,25 @@ export const Home = () => {
 
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
-	  { accounts.length <=0 && <p>Redirecting...</p> }
-	  { accounts.length > 0 && <div>
-        <HeaderComponent />
-        <AccountInfo accounts={accounts} />
-        <CredentialsInfo
-          credentials={credentials}
-          onDeleteCredential={handleCredentialDelete}
-        />
-	  </div>
-	  }
+      {accounts.length <= 0 && <p>Redirecting...</p>}
+      {accounts.length > 0 && (
+        <div>
+          <HeaderComponent />
+          <AccountInfo accounts={accounts} />
+          <TextField
+            label="Query"
+            type="text"
+            multiline={true}
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          />
+          <hr />
+          <CredentialsInfo
+            credentials={credentials}
+            onDeleteCredential={handleCredentialDelete}
+          />
+        </div>
+      )}
     </div>
   );
 };
