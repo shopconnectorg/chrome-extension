59e350e51ee2a57dc69a5034567d85a9467a17a1
benbaessler <mail@benbaessler.com>
2023-10-25 18:13:02 +0100

    Handle no deals on UI

--- src/components/DealList/index.jsx
+++ src/components/DealList/index.jsx
@@ -8,14 +8,18 @@ import {
   AccordionTrigger,
 } from "../ui/accordion";
 import { Checkbox } from "../ui/checkbox";
-import { useShopConnectStore } from '../../utils/store';
+import { useShopConnectStore } from "../../utils/store";
 import { useShopConnect } from "../../utils/hooks";
 
 export default function DealList() {
   const [error, setError] = useState(null);
   const promotions = useShopConnectStore((state) => state.promotions);
-  const applyingPromotion = useShopConnectStore((state) => state.applyingPromotion);
-  const promotionApplied = useShopConnectStore((state) => state.promotionApplied);
+  const applyingPromotion = useShopConnectStore(
+    (state) => state.applyingPromotion
+  );
+  const promotionApplied = useShopConnectStore(
+    (state) => state.promotionApplied
+  );
   const sc = useShopConnect();
 
   const applyPromotion = async (event, promotion) => {
@@ -31,61 +35,71 @@ export default function DealList() {
 
   const promotionButton = (promotion) => {
     if (promotionApplied === promotion.id) {
-      if (applyingPromotion) return (
-        <span>Applying...</span>
-      );
-      return (
-        <span>Applied</span>
-      );
+      if (applyingPromotion) return <span>Applying...</span>;
+      return <span>Applied</span>;
     }
 
     return (
-      <Button onClick={(event) => applyPromotion(event, promotion)}>Apply</Button>
+      <Button onClick={(event) => applyPromotion(event, promotion)}>
+        Apply
+      </Button>
     );
-  }
+  };
 
   return (
-    <div>
-      {error && <div style={{ padding: "20px", color: "red" }}>{error}</div>}
-      {promotions.map((promotion, index) => (
-        <div key={promotion.id}>
-          <Accordion type="single" collapsible>
-            <AccordionItem value="item-1">
-              <AccordionTrigger>
-                <div className="w-full flex p-4 justify-between items-center text-left">
-                  <div className="flex space-x-4 items-center">
-                    <img
-                      src={promotion.image}
-                      alt="deal"
-                      className="w-20 h-20 rounded-lg"
-                    />
-                    <div className="flex flex-col items-start	space-y-1 pr-4">
-                      <Badge variant="outlined">{promotion.discountType === 'percentage' ? `${promotion.discount}%`: `$${promotion.discount}`} OFF</Badge>
-                      <span className="font-bold">{promotion.title}</span>
-                      <span className="text-sm">Expires in 7 days</span>
+    <>
+      {promotions.length > 0 ? (
+        <div>
+          {promotions.map((promotion, index) => (
+            <div key={promotion.id}>
+              <Accordion type="single" collapsible>
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>
+                    <div className="w-full flex p-4 justify-between items-center text-left">
+                      <div className="flex space-x-4 items-center">
+                        <img
+                          src={promotion.image}
+                          alt="deal"
+                          className="w-20 h-20 rounded-lg"
+                        />
+                        <div className="flex flex-col items-start	space-y-1 pr-4">
+                          <Badge variant="outlined">
+                            {promotion.discountType === "percentage"
+                              ? `${promotion.discount}%`
+                              : `$${promotion.discount}`}{" "}
+                            OFF
+                          </Badge>
+                          <span className="font-bold">{promotion.title}</span>
+                          <span className="text-sm">Expires in 7 days</span>
+                        </div>
+                      </div>
+                      {promotionButton(promotion)}
                     </div>
-                  </div>
-                  {promotionButton(promotion)}
-                </div>
-              </AccordionTrigger>
-              <AccordionContent value="item-1">
-                <div className="pl-3">
-                  <div className="flex items-center space-x-2">
-                    <Checkbox id="terms" checked={true} disabled />
-                    <label
-                      htmlFor="terms"
-                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                    >
-                      {promotion.requirement}
-                    </label>
-                  </div>
-                </div>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-          {index < promotions.length - 1 && <hr className="my-4" />}
+                  </AccordionTrigger>
+                  <AccordionContent value="item-1">
+                    <div className="pl-3">
+                      <div className="flex items-center space-x-2">
+                        <Checkbox id="terms" checked={true} disabled />
+                        <label
+                          htmlFor="terms"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                          {promotion.requirement}
+                        </label>
+                      </div>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+              {index < promotions.length - 1 && <hr className="my-4" />}
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      ) : (
+        <div className="flex justify-center mt-10 text-slate-400">
+          <span>There are no active deals on this site.</span>
+        </div>
+      )}
+    </>
   );
 }
